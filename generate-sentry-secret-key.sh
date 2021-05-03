3ac4f566123ecc43b491b4858863faf110fef0ba
ItsAlex <me@itsalex.fr>
2021-05-03 12:04:50 +0200

    chore: reinit project from start

--- generate-sentry-secret-key.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -f env ]; then
-  echo "Generate SENTRY_SECRET_KEY in ./env file"
-  echo "SENTRY_SECRET_KEY=$(docker run --rm sentry:9.0.0 sentry config generate-secret-key)" > env
-fi
